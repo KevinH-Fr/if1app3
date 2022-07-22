2741ae26d9a07b8ca895c113fdc4011dbd67b890
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-22 23:53:40 +0200

    modif null true de curcuit

--- db/migrate/20220722214623_add_circuit_to_events.rb
+++ db/migrate/20220722214623_add_circuit_to_events.rb
@@ -1,5 +1,5 @@
 class AddCircuitToEvents < ActiveRecord::Migration[7.0]
   def change
-    add_reference :events, :circuit, null: false, foreign_key: true
+    add_reference :events, :circuit, null: true, foreign_key: true
   end
 end
