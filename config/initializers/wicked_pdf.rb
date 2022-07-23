b26bf60c2e37902a5efff8bd06ec419319865bbc
KevinH-Fr <kevin.hoffman.france@gmail.com>
2022-07-23 18:58:06 +0200

    order classement pilotes

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,14 +1,14 @@
 
 # fonctionne en prod : 
 
-#WickedPdf.config ||= {}
-#WickedPdf.config.merge!({
-#  layout: "pdf.html.erb",
-#}) 
+WickedPdf.config ||= {}
+WickedPdf.config.merge!({
+  layout: "pdf.html.erb",
+}) 
 
 
 # focntionne en dev :
 
-WickedPdf.config = {
-  :exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
-}
+#WickedPdf.config = {
+#  :exe_path => 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe'
+#}
